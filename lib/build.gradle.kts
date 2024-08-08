74abd2b9a3996461af99872c387f382b77e372d5
lennoard <lennoardrai@gmail.com>
2024-08-07 22:29:39 -0300

    refactor: project Android run configuration

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 val snappierGitHubUser: String by project
 val snappierGitHubToken: String by project
 val libraryGroup = "br.com.androidvip.snappier"
-val libraryVersion = "1.0.3"
+val libraryVersion = "1.0.4-alpha01"
 
 group = libraryGroup
 version = libraryVersion
